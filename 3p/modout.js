684bd76e048d1a1f090654d5720d6abecc1bd1d4
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2019-06-12 01:14:12 -0500

    Delete modout.js

--- 3p/modout.js
+++
@@ -1,7 +0,0 @@
-var Module={}
-    Module.methodOne= function(){
-        console.log('performing a complex task..')
-    }
-    Module.methodTwo= function(){
-        console.log('performing a complex task (2)')
-    }
