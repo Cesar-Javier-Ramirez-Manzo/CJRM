3a4e928edc4b41d3a6bf61e559b9ae9107c86505
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2019-06-12 01:12:06 -0500

    Delete pal.js

--- 3p/htm/pal.js
+++
@@ -1,9 +0,0 @@
-function pal(){
-    
-    let t = document.getElementById("txt")
-   var cont=0
-    if(element==' '){
-        cont++
-    }
-    document.getElementById("out").innerHTML = cont
-}
