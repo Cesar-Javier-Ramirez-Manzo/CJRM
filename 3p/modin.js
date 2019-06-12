8175d0bd02d7b8a36bbb1485dd4d5f79eeebbdaa
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2019-06-12 01:14:06 -0500

    Delete modin.js

--- 3p/modin.js
+++
@@ -1,8 +0,0 @@
-var Module={
-    methodOne: function(){
-        comsole.log('performing a complex task..')
-    },
-    methodTwo: function(){
-        console.log('performing a complex task (2)')
-    }
-}
